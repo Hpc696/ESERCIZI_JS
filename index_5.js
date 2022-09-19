1c8e2a526b3d8430dc8f33ea1fb6ac540abb782e
Matteo_G <honrop.giochi@gmail.com>
2022-09-19 15:09:17 +0200

    esercizio 8 non riuscito

---
+++ index_5.js
@@ -0,0 +1,12 @@
+const prompt=require('prompt-sync')();
+//8. numero in input K, ritornate tutti i numeri primi compresi tra 1 e K
+let k= prompt("Inserisci un numero");
+while (k=>i){
+    for(i=0;i<=k;i++){
+        if(i% ==0 i ){
+            primo= i;
+            console.log(primo);
+        }
+    }
+    i--;
+}
---
+++ index_6.js
@@ -0,0 +1,2 @@
+const prompt=require('prompt-sync')();
+//9. permetta di indovinare una parola da una lista di parole
