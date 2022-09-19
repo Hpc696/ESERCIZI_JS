3c6a6243259cec6d4fdbd03e290fe198a2702040
Matteo_G <honrop.giochi@gmail.com>
2022-09-19 13:03:35 +0200

    esercizio 5 base

---
+++ index_2.js
@@ -0,0 +1,13 @@
+const prompt=require('prompt-sync')();
+
+//5. numero in input M, stampare somma dei numeri divisibili x 3 fino al numero M (come prima ma usare modulo per determinare num divisibili per 3)
+let m= prompt("Inserisci un numero");
+let somma=0;
+for(let i=0; i<=m; i++){
+    if (i%3==0){
+        somma= i + somma
+    }
+};
+console.log(somma);
+
+//6.
