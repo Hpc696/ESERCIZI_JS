6d6c95a3f7425c926fa8a635bd4c2b98c2fc6fa3
Matteo_G <honrop.giochi@gmail.com>
2022-09-21 19:40:52 +0200

    esercizio 7 base+bonus completo

--- index_4.js
+++ index_4.js
@@ -1,11 +1,15 @@
 const prompt=require('prompt-sync')();
 //7. numero in input J, stampate tutte le tabelline da 2 a J
-let j=prompt("Inserisci un numero");
-j=parseInt(j);
+let j= parseInt(prompt("Inserisci un numero"));
 let tabb=0; let tabbcorr=0;
+//bonus: da input quale numero escludere dalla tabella
+let escluso= parseInt(prompt("Inserisci un numero da escludere dalla tabella"));
 while (2 <= j) /* ciclo per limitare tabelline da 2 a J*/ {
-    console.log("Tabellina di "+ j + "è");
+    console.log("Tabellina di "+ j + " è");
     for (let i=1 ; i<=10; i++){
+        if(i==escluso){
+            continue;
+        }
         tabbcorr= j;
         tabb= j;
         tabb *= i; //incr moltipl
