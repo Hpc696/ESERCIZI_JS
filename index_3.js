586489f19660fa879df6f8f289bed9137dfab957
Matteo_G <honrop.giochi@gmail.com>
2022-09-21 19:26:42 +0200

    esercizio 6 base+bonus completo

--- index_3.js
+++ index_3.js
@@ -1,21 +1,35 @@
 const prompt=require('prompt-sync')();
 //6. creare una calcolatrice: prendendo in input 2 numeri e l'operatore da usare
-let primon= parseInt(prompt("Inserisci il primo numero"));
-let secondon= parseInt(prompt("Inserisci il secondo numero"));
+let primonum= parseInt(prompt("Inserisci il primo numero"));
+let secondonum= parseInt(prompt("Inserisci il secondo numero"));
 let risultato=0;
-
-switch(prompt("Per addizione premi + ; Per sottrazione premi - ; Per moltiplicazione premi * ; Per divisione premi /")) { //uso switch per distinguere add,sottr,moltip,divis
+/*bonus: implementare funzionalità memoria*/
+let mem='m';
+while(mem == 'm'){
+switch(prompt("Per addizione premi + ; Per sottrazione premi - ; Per moltiplicazione premi * ; Per divisione premi / ; Per elevazione a potenza primonume^secondonum premere ^ ; Per radice quadrata del 1^numero premere sqrt ; Per radice quadrata del 2^numero premere sqrt2 ;  ")) { //uso switch per distinguere add,sottr,moltip,divis
     case "+":
-    risultato= primon + secondon ; break;
+    risultato= primonum + secondonum ; break;
     case "-":
-    risultato= primon - secondon ; break;
+    risultato= primonum - secondonum ; break;
     case "*":
-    risultato= primon * secondon ; break;
+    risultato= primonum * secondonum ; break;
     case "/":
-    risultato= primon / secondon ; break;
-};
+    risultato= primonum / secondonum ; break;
+//parte bonus aggiungere radice quadrata e potenza
+    case "^":
+    risultato= primonum ** secondonum; break; 
+    case "sqrt":
+    risultato= Math.sqrt(primonum) ; break;
+    case "sqrt2":
+    risultato= Math.sqrt(secondonum) ; break;
+    default:
+    console.log("Che ca**o di operatore hai digitato?!");
+}; }
 console.log("Il risultato è" + risultato);
-/*parte bonus -implementate la funzionalita' memoria
--aggiungere radice quadrata e potenza */
-
-
+/* mem=prompt("Premi m se vuoi fare altra operazione");
+if (mem =='m' ){
+    //funzione che ripete il while con lo switch
+} 
+if (mem != 'm'){
+    break;
+} */
