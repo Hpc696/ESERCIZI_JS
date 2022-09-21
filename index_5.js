9861add8e7d831f24d7cd28cf1a567a7390dbde7
Matteo_G <honrop.giochi@gmail.com>
2022-09-22 01:17:46 +0200

    esercizio 8 base (miglioramento?)

--- index_5.js
+++ index_5.js
@@ -5,7 +5,8 @@ console.log("Uè aspè cerco tutti i numeri primi fino a "+K);
 let quant=0;  //contatore x la quantità di nums primi
 for (let i=2; i<=K; i++) { //ciclo numeri che vanno fino a K 
 	let contatore= true; //contatore booleano x riconoscere numero primo sì/no
-	for(let divis=2; divis<i; divis++){ //ciclo x divisori dei numeri(i)
+	//x migliorare prestazioni? divis<=radice quadrata dei numeri(i)?...così metà dei for?
+	for(let divis=2; divis<i/*divis<=Math.sqrt(i)*/; divis++){ //ciclo x divisori dei numeri(i)
 		if(i%divis === 0){
 			contatore=false;
 			break;
