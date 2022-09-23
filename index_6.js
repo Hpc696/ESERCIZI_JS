e557af3ec3bef7f9b05a2f8592e7872316e1dce8
Matteo_G <honrop.giochi@gmail.com>
2022-09-23 09:28:42 +0200

    esercizio 9 base

--- index_6.js
+++ index_6.js
@@ -1,10 +1,11 @@
 const prompt=require('prompt-sync')();
 //9. permetta di indovinare una parola da una lista di parole
+const lista= new Array("casso" , "zizzona" , "didio" , "battipaglia" , "muoio");
 let parola= prompt("Inserisci una parola ca**one"); 
-let lista= ["casso" , "zizzona" , "di" , "battipaglia" , "muoio"]
-for(i=0; i<5; i++){
-    if( ){
+for(i=0; i<lista.length; i++){
+    if(parola==lista[i]){
         console.log("HAI INDOVINATO COJ*NE");
+        break;
     } else {
         console.log("FAI SCH1FO");
     }
