08e0f93a8b446aaaff27e2690ac5ef2c11a35fca
Matteo_G <honrop.giochi@gmail.com>
2022-09-27 00:37:51 +0200

    esercizio 12 parte bonus mancante

--- index_9.js
+++ index_9.js
@@ -5,17 +5,15 @@ Ritornate quanti mesi intercorrono tra oggi e l'anno inserito dall'utente (consi
 Ritornate quante settimane passano tra la data indicata e oggi (considerate primo ottobre) */
 
 let n=parseInt(prompt("Inserisci l'anno: ")); let check= false;
-let risposta=prompt("Secondo te è bisestile? Premi Y per yes o N per no: "); 
-if(risposta=!"Y" || risposta=!"N"ß){
-    console.log("NON HAI DIGITATO BENEEEE!!!");
-} else if((risposta=="Y" && check==true) || (risposta=="N" && check==false)){
+let risposta=prompt("Secondo te è bisestile? Premi y per sì o n per no: "); 
+if((n%400==0) || (n%4==0 && !(n%100==0))){
+    console.log("L'anno "+n+" è bisestile"); check=true; 
+} else {
+    console.log("L'anno "+n+" non è bisestile"); check=false;
+} 
+if((risposta==="y" && check==true) || (risposta==="n" && check==false)){
     console.log("Hai indovinato yeee!");
 }  else {
     console.log("Non hai indovinato sf1ghat0!");
-}  if((n%400==0) || (n%4==0 && !(n%100==0))){
-    console.log("L'anno "+n+" è bisestile"); check=true; 
-} else{
-    console.log("L'anno "+n+" non è bisestile"); check=false;
-}
-
-//era fattibile anche con switch case anzi meglio...
+}  
+//era fattibile anche con switch case anzi meglio...possibile mettere caso default:'non hai digitato bene belin!'
