69dcbc7c81c732640931dba5a340a3724201aad5
Matteo_G <honropgiochi@gmail.com>
2022-09-27 10:12:57 +0200

    esercizio 12 base + bonus completi

--- index_9.js
+++ index_9.js
@@ -3,7 +3,6 @@ const prompt=require('prompt-sync')();
 Fare in modo che la risposta non venga data immediatamente ma date all'utente la possibilita' di indovinare;
 Ritornate quanti mesi intercorrono tra oggi e l'anno inserito dall'utente (considerate primo ottobre);
 Ritornate quante settimane passano tra la data indicata e oggi (considerate primo ottobre) */
-
 let n=parseInt(prompt("Inserisci l'anno: ")); let check= false;
 let risposta=prompt("Secondo te è bisestile? Premi y per sì o n per no: "); 
 if((n%400==0) || (n%4==0 && !(n%100==0))){
@@ -17,3 +16,26 @@ if((risposta==="y" && check==true) || (risposta==="n" && check==false)){
     console.log("Non hai indovinato sf1ghat0!");
 }  
 //era fattibile anche con switch case anzi meglio...possibile mettere caso default:'non hai digitato bene belin!'
+const datacorrente= new Date();
+let anno=n.toString();
+let annoinserito=new Date(anno);
+//imposto '1^ottobre anno' come data inserita dall'utente
+annoinserito.setDate(1);
+annoinserito.setMonth(9);
+console.log("Hai inserito: "+annoinserito);
+let mesi=diffmesi(datacorrente, annoinserito);
+console.log("Tra oggi e l'anno inserito (1^Ottobre) intercorrono "+mesi+ " mesi");
+let settimane=diffsett(datacorrente, annoinserito);
+console.log("Tra oggi e l'anno inserito (1^Ottobre intercorrono "+settimane+ " settimane");
+//funzione x differenza mesi
+function diffmesi(datacorrente, annoinserito){ 
+    let diff=(datacorrente.getTime()-annoinserito.getTime())/1000; 
+    diff/= (60 * 60 * 24 * 7 * 4); 
+    return Math.abs(Math.round(diff)); 
+}
+//funzione x differenza settimane
+function diffsett(datacorrente, annoinserito){
+    let diff=(datacorrente.getTime()-annoinserito.getTime())/1000;
+    diff/= (60 * 60 *24 * 7) ;
+    return Math.abs(Math.round(diff));
+}
