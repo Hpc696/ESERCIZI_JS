f732a48b9620715fa959e04ee84d64a9a93b9161
Matteo_G <honropgiochi@gmail.com>
2022-09-27 16:30:22 +0200

    esercizio 9 base + bonus completo

--- index_6.js
+++ index_6.js
@@ -1,12 +1,52 @@
 const prompt=require('prompt-sync')();
-//9. permetta di indovinare una parola da una lista di parole
-const lista= new Array("casso" , "zizzona" , "didio" , "battipaglia" , "muoio");
-let parola= prompt("Inserisci una parola ca**one"); 
-for(i=0; i<lista.length; i++){
-    if(parola==lista[i]){
-        console.log("HAI INDOVINATO COJ*NE");
+//9. permetta di indovinare una parola da una lista di parole. - Bonus: implementare impiccato
+const lista = new Array("didio", "fraberto", "brafe", "minne");
+let parola= prompt("Inserisci una parola ca**one: "); 
+let check = false;
+let lettereIndovinate = "";
+
+for (let i=0; i<lista.length; i++){
+    if(parola==lista.any){
+        check=true;
+        console.log("Easyyy!!!")
         break;
-    } else {
-        console.log("FAI SCH1FO");
+        } else{
+            check==false;  
+            console.log("Looser L!!!");     
+        } 
+}
+if (check==false){
+    let parolaImpiccato= parola.split("");
+    let lettera;
+    let parolaIndovinata=[];
+    for(let i=0; i<parolaImpiccato.length; i++){
+        parolaIndovinata[i]=" _ ";
     }
+
+    let cont=parolaImpiccato.length;
+    let vite=3;
+    while(cont > 0){
+        lettera=prompt("Indovina una lettera:       ");
+        let i=parolaImpiccato.indexOf(lettera);
+        lettereIndovinate="";
+        if(i>=0){
+            parolaIndovinata[i]=parolaImpiccato[i];
+            parolaImpiccato[i]='$';
+            cont--;
+        }
+        if(i==-1){
+            vite--;
+        }
+        for(const x of parolaIndovinata){
+            lettereIndovinate+= x+ " ";
+        }
+        console.log("Situazione :" + lettereIndovinate + "   ");
+        if(vite==0){
+            console.log("GAME OVER L");
+            break;
+        }
+        console.log("Hai "+vite+" vite rimaste");
+    }
+    if(vite > 0)
+        console.log("Sei un animaleh!")
 }
